2ffc1b85c7b683f9bdba0f6eb2bad51a51f7e577
Ilya Likhobabin <ilya.likhobabin@gmail.com>
2012-02-15 18:21:38 +0400

    Add test_1

---
+++ Tests/tstOpeningBigFiles/Source/main.cpp
@@ -0,0 +1,26 @@
+#include <fsPrecompile.h>
+//
+#include <fsBuffer.h>
+#include <fsBitGenerator.h>
+#include <fsDataFormer.h>
+////
+#ifdef __BORLANDC__
+   #pragma option -w -O2 -vi- -b -6 -k -a8 -pc -ff
+#endif
+//
+#ifdef _MSC_VER
+   #pragma pack(8)
+   #pragma warning(disable:4355)
+   #ifdef min
+      #undef min
+   #endif
+   #ifdef max
+      #undef max
+   #endif
+#endif
+/////
+int main(int /*argc*/, const char** /*args*/)
+{
+ 
+ return(0);
+}
---
+++ Tests/tstOpeningBigFiles/nbproject/Makefile-Debug.mk
@@ -0,0 +1,83 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/Source/main.o
+
+
+# C Compiler Flags
+CFLAGS=-m32
+
+# CC Compiler Flags
+CCFLAGS=-m32
+CXXFLAGS=-m32
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-L../../FileSignature/dist/Debug/GNU-Linux-x86 -lfilesignature
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tstopeningbigfiles
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tstopeningbigfiles: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tstopeningbigfiles ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/Source/main.o: Source/main.cpp 
+	${MKDIR} -p ${OBJECTDIR}/Source
+	${RM} $@.d
+	$(COMPILE.cc) -g -Wall -I. -IInclude -I../../FileSignature/Include -MMD -MP -MF $@.d -o ${OBJECTDIR}/Source/main.o Source/main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tstopeningbigfiles
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
