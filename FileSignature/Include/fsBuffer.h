851e6723e3e8e84b6afc73934142dbf2d0b664aa
Ilya Likhobabin <ilya.likhobabin@gmail.com>
2012-02-15 17:42:27 +0400

    Add Encode Buffer Class

---
+++ FileSignature/Include/fsBuffer.h
@@ -0,0 +1,55 @@
+#ifndef __FSBUFFER_H__
+#define	__FSBUFFER_H__
+////
+#ifdef _MSC_VER
+#pragma pack(push,8)
+#endif
+//
+#ifdef __BORLANDC__
+#pragma option push -w -O2 -vi- -b -6 -k -a8 -pc -ff
+#endif
+////
+class TBuffer
+{
+public:
+    TBuffer(void);
+    TBuffer(const TByte data[], unsigned long /*__FSize*/);
+    TBuffer(unsigned long /*__FSize*/);
+    ///
+    unsigned long getSize(void ) const 
+    {
+        return(FSize);
+    }
+    TByte* getData(void ) const 
+    {
+        return(FData);
+    }
+    //
+    void doFill(const TByte data[], unsigned long /*__FSize*/);
+    void doClose(void );
+    ///    
+    ~TBuffer(void);
+    
+private:
+    TByte* FData; 
+    unsigned long FSize;
+    //
+    void Resize(unsigned long /*__FSize*/);
+    void Fill(const TByte __FFillInData[], unsigned long /*__FSize*/);
+    //
+    void setSize(unsigned long __FNewSize)
+    {
+        FSize = __FNewSize;
+    }
+};
+////
+#ifdef _MSC_VER
+#pragma pack(pop)
+#endif
+//
+#ifdef __BORLANDC__
+#pragma option pop
+#endif
+////
+#endif	/*__FSBUFFER_H__ */
+
---
+++ FileSignature/Source/fsBuffer.cpp
@@ -0,0 +1,97 @@
+#include "fsPrecompile.h"
+//
+#include "fsBuffer.h"
+////
+#ifdef __BORLANDC__
+   #pragma option -w -O2 -vi- -b -6 -k -a8 -pc -ff
+#endif
+//
+#ifdef _MSC_VER
+   #pragma pack(8)
+   #pragma warning(disable:4355)
+   #ifdef min
+      #undef min
+   #endif
+   #ifdef max
+      #undef max
+   #endif
+#endif
+/////
+TBuffer::TBuffer(void):
+FData(NULL),
+FSize(0x0L)
+{
+}
+//
+TBuffer::TBuffer(unsigned long __FSize):
+FData(NULL),
+FSize(__FSize)
+{
+ TByte nullChar='0';
+ //
+ FData = new TByte[__FSize];
+ memset(getData(), getSize(), nullChar);
+}
+//
+TBuffer::TBuffer(const TByte __FData[], unsigned long __FSize):
+FData(NULL),
+FSize(0x0L)
+{
+   doFill(__FData, __FSize);
+}
+//
+void TBuffer::doFill(const TByte __FData[], unsigned long __FSize)
+{
+ if(__FSize > getSize()  )
+    Resize(__FSize);
+ //
+ Fill(__FData, __FSize);
+ setSize(__FSize);
+}
+//
+void TBuffer::doClose(void )
+{
+ if(NULL != FData)
+  {
+    delete(FData);
+    FData = NULL;
+    setSize(0x0L);
+  }
+}
+///    
+TBuffer::~TBuffer(void)
+{
+ doClose();
+}
+//***************************//
+void TBuffer::Resize(unsigned long __FNewSize)
+{
+   if(__FNewSize > getSize())
+   {
+      TByte nullChar = '0';
+      TByte* newData = NULL;
+      newData = new TByte[__FNewSize];
+      //
+      TByte* oldData = getData();
+      //
+      if (0x0 != newData)
+      {
+         FSize=__FNewSize;
+      }
+      else
+      {
+         FSize=0x0;
+      }
+      //
+      FData = newData;
+      memset(getData(), getSize(), nullChar);
+      //
+      delete[] oldData;
+      oldData = NULL;
+   }
+}
+//
+void TBuffer::Fill(const TByte __FFillInData[], unsigned long __FSize)
+{
+ memcpy(getData(), __FFillInData, getSize());
+}
