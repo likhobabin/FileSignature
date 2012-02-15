b63ff0b060c865ffbc78f8941e818fb7116a43ea
Ilya Likhobabin <ilya.likhobabin@gmail.com>
2012-02-15 19:06:12 +0400

    modified:   FileSignature/Source/fsBuffer.cpp

--- FileSignature/Source/fsBuffer.cpp
+++ FileSignature/Source/fsBuffer.cpp
@@ -3,95 +3,102 @@
 #include "fsBuffer.h"
 ////
 #ifdef __BORLANDC__
-   #pragma option -w -O2 -vi- -b -6 -k -a8 -pc -ff
+#pragma option -w -O2 -vi- -b -6 -k -a8 -pc -ff
 #endif
 //
 #ifdef _MSC_VER
-   #pragma pack(8)
-   #pragma warning(disable:4355)
-   #ifdef min
-      #undef min
-   #endif
-   #ifdef max
-      #undef max
-   #endif
+#pragma pack(8)
+#pragma warning(disable:4355)
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
 #endif
 /////
+
 TBuffer::TBuffer(void):
 FData(NULL),
 FSize(0x0L)
 {
 }
 //
-TBuffer::TBuffer(unsigned long __FSize):
+
+TBuffer::TBuffer(unsigned long __FSize) :
 FData(NULL),
 FSize(__FSize)
 {
- TByte nullChar='0';
- //
- FData = new TByte[__FSize];
- memset(getData(), getSize(), nullChar);
+    TByte nullChar = '0';
+    //
+    FData = new TByte[__FSize];
+    memset(getData(), getSize(), nullChar);
 }
 //
-TBuffer::TBuffer(const TByte __FData[], unsigned long __FSize):
+
+TBuffer::TBuffer(const TByte __FData[], unsigned long __FSize) :
 FData(NULL),
 FSize(0x0L)
 {
-   doFill(__FData, __FSize);
+    doFill(__FData, __FSize);
 }
 //
+
 void TBuffer::doFill(const TByte __FData[], unsigned long __FSize)
 {
- if(__FSize > getSize()  )
-    Resize(__FSize);
- //
- Fill(__FData, __FSize);
- setSize(__FSize);
+    if (__FSize > getSize())
+        Resize(__FSize);
+    //
+    Fill(__FData, __FSize);
+    setSize(__FSize);
 }
 //
-void TBuffer::doClose(void )
+
+void TBuffer::doClose(void)
 {
- if(NULL != FData)
-  {
-    delete(FData);
-    FData = NULL;
-    setSize(0x0L);
-  }
+    if (NULL != FData)
+    {
+        delete(FData);
+        FData = NULL;
+        setSize(0x0L);
+    }
 }
 ///    
 TBuffer::~TBuffer(void)
 {
- doClose();
+    doClose();
 }
 //***************************//
 void TBuffer::Resize(unsigned long __FNewSize)
 {
-   if(__FNewSize > getSize())
-   {
-      TByte nullChar = '0';
-      TByte* newData = NULL;
-      newData = new TByte[__FNewSize];
-      //
-      TByte* oldData = getData();
-      //
-      if (0x0 != newData)
-      {
-         FSize=__FNewSize;
-      }
-      else
-      {
-         FSize=0x0;
-      }
-      //
-      FData = newData;
-      memset(getData(), getSize(), nullChar);
-      //
-      delete[] oldData;
-      oldData = NULL;
-   }
+    if (__FNewSize > getSize())
+    {
+        TByte nullChar = '0';
+        TByte* newData = NULL;
+        newData = new TByte[__FNewSize];
+        //
+        TByte* oldData = getData();
+        //
+        if (NULL != newData)
+        {
+            FSize = __FNewSize;
+            FData = newData;
+            memset(getData(), getSize(), nullChar);
+        }
+        else
+        {
+            FSize = 0x0;
+            FData = NULL;
+        }
+        //
+        delete[] oldData;
+        oldData = NULL;
+    }
 }
 //
 void TBuffer::Fill(const TByte __FFillInData[], unsigned long __FSize)
 {
- memcpy(getData(), __FFillInData, getSize());
+    TByte nullChar = '0';
+    memset(getData(), getSize(), nullChar);
+    memcpy(getData(), __FFillInData, getSize());
 }
